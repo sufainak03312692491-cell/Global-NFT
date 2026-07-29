16d78232b7212ad548afd1bfc0364f54900ea50c
sufainak03312692491-cell <sufainak03312692491@gmail.com>
2026-07-30 00:16:25 +0500

    Add event listener for button click alert

---
+++ app.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.querySelector("button");
+    
+    button.addEventListener("click", () => {
+        alert("Task completed! +10 Points added.");
+    });
+});
