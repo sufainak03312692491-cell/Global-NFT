6aba0a2ce1eee8f7818bbf97daefc49720de11a0
sufainak03312692491-cell <sufainak03312692491@gmail.com>
2026-07-30 00:58:21 +0500

    Add root route to serve backend status message

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+  res.send('Global NFT Backend is running!');
+});
 
 app.use(express.static(__dirname));
 
