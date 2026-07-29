e9536328611d81bc8a4233fde4f69b1e7a6999d7
sufainak03312692491-cell <sufainak03312692491@gmail.com>
2026-07-30 01:04:32 +0500

    Add API endpoint to fetch sample NFTs

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const PORT = process.env.PORT || 5000;
 app.get('/', (req, res) => {
   res.send('Global NFT Backend is running!');
 });
+app.get('/api/nfts', (req, res) => {
+  const sampleNfts = [
+    { id: 1, name: 'Global NFT #1', price: '0.1 ETH' },
+    { id: 2, name: 'Global NFT #2', price: '0.2 ETH' }
+  ];
+  res.json(sampleNfts);
+});
 
 app.use(express.static(__dirname));
 
