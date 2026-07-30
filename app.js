4f26af3abc0c0808d49ec63eae10acd499e1579f
sufainak03312692491-cell <sufainak03312692491@gmail.com>
2026-07-30 12:25:49 +0500

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const button = document.querySelector("button");
-    
-    button.addEventListener("click", () => {
-        alert("Task completed! +10 Points added.");
-    });
-});
